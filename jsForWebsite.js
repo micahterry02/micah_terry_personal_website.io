81a62dffdfc44114d48630585e40fdb237722455
Micah Terry <micahter@buffalo.edu>
2024-07-18 07:46:42 -0400

    haha haven't pushed in weeks

--- jsForWebsite.js
+++ jsForWebsite.js
@@ -1,3 +1,37 @@
+let welcomeScreen =document.querySelector(".welcomeScreen");
+let logo =document.querySelector(".welcomeScreen-header");
+let logoSpan=document.querySelectorAll(".logo");
+window.addEventListener("DOMContentLoaded", ()=>{
+    setTimeout(()=>{
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            },(idx+1)*400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span,idx)=>{
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                },(idx+1)*50)
+            })
+        },2000)
+
+        setTimeout(()=>{
+            welcomeScreen.style.top= '-100vh';
+        },2300)
+    })
+})
+
+
+
+
+
+
+
+
+
 const people=[
     {
     firstName:"john",
