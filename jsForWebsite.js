78156c62b86be92dc3f7b4d0c477dc219d77ad60
Micah Terry <micahter@buffalo.edu>
2024-08-05 22:07:18 -0400

    all updated now

--- jsForWebsite.js
+++ jsForWebsite.js
@@ -23,33 +23,3 @@ window.addEventListener("DOMContentLoaded", ()=>{
         },2300)
     })
 })
-
-
-
-
-
-
-
-
-
-const people=[
-    {
-    firstName:"john",
-    age:30,
-    weight:170
-},
-{
-    firstName:"sally",
-    age:29,
-    weight:5000
-}
-]
-function printr(){
-    for (let person of people){
-        console.log(person.age)
-    }
-}
-printr();
-const mainbody = document.getElementById("mainBody");
-const mainbodyTwo= document.querySelectorAll('li');
-console.log(mainbodyTwo);
